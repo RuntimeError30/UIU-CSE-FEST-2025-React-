d7b98801a81dfa9ce52d1930f3f163a309c0b04a
RuntimeError30 <mdkfahim@gmail.com>
2024-11-24 14:09:35 -0500

    Hero Section Done Alhamdulillah

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Hero from './components/Hero'
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      <Hero/>
+      <Hero />
+      <section className='z-0 min-h-screen bg-blue-500' />
     </main>
   )
 }
