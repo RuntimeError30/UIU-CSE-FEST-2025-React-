cde3ecd9df78e8fab8886dd750c5a3a844adf689
RuntimeError30 <mdkfahim@gmail.com>
2024-11-24 10:26:39 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-5xl text-orange-500'>Welcome to UIU CSE FEST 2025</h1>
+    </main>
+  )
+}
+
+export default App
