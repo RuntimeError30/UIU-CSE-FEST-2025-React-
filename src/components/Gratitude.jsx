583760fb92527f6d248d25f2b08aa20d210e6793
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-11-27 00:28:31 +0600

    member info link added

--- src/components/Gratitude.jsx
+++ src/components/Gratitude.jsx
@@ -31,21 +31,23 @@ const Gratitude = () => {
   return (
     <div className="min-h-screen  bg-black text-white font-sans">
       <main className="container mx-auto px-4 py-12">
-        <h1 className="text-4xl md:text-5xl mt-10 font-bold text-center mb-4 text-orange-500">
+      
+        <h1 className="font-zentry text-4xl md:text-5xl mt-10 font-bold text-center mb-4 text-orange-500">
           Meet Our Amazing Team
         </h1>
-        <p className="text-xl text-center mb-12 text-gray-300">
+        <p className="font-robert-regular text-xl text-center mb-12 text-gray-300">
           The talented individuals behind UIU CSE FEST 2025&apos;s success
         </p>
 
         <section className="mb-16">
-          <h2 className="text-3xl font-bold text-center mb-8 text-orange-500">Organizing Committee</h2>
+          <h2 className="font-zentry text-3xl font-bold text-center mb-8 text-orange-500">Organizing Committee</h2>
 
           <div className="mb-12">
-            <h3 className="text-2xl font-bold mb-6 text-center">Chief Patron</h3>
+            <h3 className="font-zentry text-3xl font-bold mb-6 text-center">Chief Patron</h3>
             <div ref={(el) => (sections.current[0] = el)} className="max-w-md mx-auto">
               <TeamMember
                 name="Dr. Md. Abul Kashem Mia"
+                link={`https://cse.uiu.ac.bd/faculty/abul-kashem/`}
                 role="Vice Chancellor, United International University"
                 imageSrc="/img/vc_sir.jpg"
               />
@@ -53,11 +55,12 @@ const Gratitude = () => {
           </div>
 
           <div className="mb-12">
-            <h3 className="text-2xl font-bold mb-6 text-center">Organizing Chairs</h3>
+            <h3 className="font-zentry text-3xl font-bold mb-6 text-center">Organizing Chairs</h3>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
               <div ref={(el) => (sections.current[1] = el)}>
                 <TeamMember
                   name="Dr. Mohammad Nurul Huda"
+                  link={`https://cse.uiu.ac.bd/faculty/mnh/`}
                   role="Professor and Head, Dept of CSE, United International University"
                   imageSrc="/img/huda_sir.jpg"
                 />
@@ -66,6 +69,7 @@ const Gratitude = () => {
               <div ref={(el) => (sections.current[2] = el)}>
                 <TeamMember
                   name="Dr. Suman Ahmmed"
+                  link={`https://cse.uiu.ac.bd/faculty/suman/`}
                   role="Associate Professor, Dept. of CSE & Director, CDIP, United International University"
                   imageSrc="/img/suman_sir.jpg"
                 />
@@ -74,11 +78,12 @@ const Gratitude = () => {
           </div>
 
           <div className="mb-12">
-            <h3 className="text-2xl font-bold mb-6 text-center">Organizing Secretaries</h3>
+            <h3 className="font-zentry text-3xl font-bold mb-6 text-center">Organizing Secretaries</h3>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
               <div ref={(el) => (sections.current[3] = el)}>
                 <TeamMember
                   name="Mr. Md. Tarek Hasan"
+                  link={`https://cse.uiu.ac.bd/faculty/tarek/`}
                   role="Lecturer, Dept of CSE, United International University"
                   imageSrc="/img/tarek_sir.jpg"
                 />
@@ -86,6 +91,7 @@ const Gratitude = () => {
               <div ref={(el) => (sections.current[4] = el)}>
                 <TeamMember
                   name="Mr. Raiyan Rahman"
+                  link={`https://cse.uiu.ac.bd/faculty/rahman-raiyan/`}
                   role="Lecturer, Dept of CSE, United International University"
                   imageSrc="/img/raiyan_sir.jpeg"
                 />
@@ -94,11 +100,12 @@ const Gratitude = () => {
           </div>
 
           <div className="mb-12">
-            <h3 className="text-2xl font-bold mb-6 text-center">Coordinators</h3>
+            <h3 className="font-zentry text-3xl font-bold mb-6 text-center">Coordinators</h3>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
               <div ref={(el) => (sections.current[5] = el)}>
                 <TeamMember
                   name="Shoib Ahmed Shourav"
+                  link={`https://cse.uiu.ac.bd/faculty/shoib/`}
                   role="Lecturer, Dept of CSE, United International University"
                   subRole="Coordinator of ICT Olympiad and Project Showcasing"
                   imageSrc="/img/shoib_sir.jpg"
@@ -107,6 +114,7 @@ const Gratitude = () => {
               <div ref={(el) => (sections.current[6] = el)}>
                 <TeamMember
                   name="Fahim Hafiz"
+                  link={`https://cse.uiu.ac.bd/faculty/hafiz-fahim/`}
                   role="Lecturer, Dept of CSE, United International University"
                   subRole="Coordinator of LFR & Robo Soccer, Treasurer, UIU CSE Fest 2025"
                   imageSrc="/img/fahim_sir.jpg"
@@ -117,11 +125,12 @@ const Gratitude = () => {
         </section>
 
         <section className="mb-16">
-          <h2 className="text-3xl font-bold text-center mb-8 text-orange-500">Web Developers</h2>
+          <h2 className=" font-zentry text-3xl font-bold text-center mb-8 text-orange-500">Web Developers</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <div ref={(el) => (sections.current[7] = el)}>
               <TeamMember
                 name="Mohammad Darain Khan"
+                link={`https://github.com/RuntimeError30`}
                 role="Technical Lead & Fullstack Developer"
                 subRole="Vice President, UIU App Forum"
                 imageSrc="/img/darain.jpg"
@@ -130,6 +139,7 @@ const Gratitude = () => {
             <div ref={(el) => (sections.current[8] = el)}>
               <TeamMember
                 name="Muddasir Faiyaj"
+                link={`https://muddasir-faiyaj.web.app/`}
                 role="Fullstack Developer"
                 subRole="Associate-Software & Innovation, UIU App Forum"
                 imageSrc="/img/faiyaj.jpg"
@@ -138,6 +148,7 @@ const Gratitude = () => {
             <div ref={(el) => (sections.current[9] = el)}>
               <TeamMember
                 name="Fatin Shadab"
+                link={`https://github.com/FatinShadab`}
                 role="Fullstack Developer"
                 subRole="Head of Programming Wing, UIU Computer Club"
                 imageSrc="/img/fatin.jpeg"
@@ -147,10 +158,11 @@ const Gratitude = () => {
         </section>
 
         <section className="mb-16">
-          <h2 className="text-3xl font-bold text-center mb-8 text-orange-500">Graphics Designer</h2>
+          <h2 className="font-zentry text-3xl font-bold text-center mb-8 text-orange-500">Graphics Designer</h2>
           <div ref={(el) => (sections.current[10] = el)} className="max-w-md mx-auto">
             <TeamMember
               name="A M Zayed Abdullah"
+              link={`https://www.facebook.com/amzayed.abdullah`}
               role="Graphics Designer"
               subRole="Head of Graphics, UIU App Forum"
               imageSrc="/img/zayed.jpg"
@@ -159,11 +171,12 @@ const Gratitude = () => {
         </section>
 
         <section className="mb-16">
-          <h2 className="text-3xl font-bold text-center mb-8 text-orange-500">Media and Communication Team</h2>
+          <h2 className="font-zentry text-3xl font-bold text-center mb-8 text-orange-500">Media and Communication Team</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             <div ref={(el) => (sections.current[11] = el)}>
               <TeamMember
                 name="Abdullah Al Masud Bhuiyan"
+                link={`https://www.facebook.com/staywithabdullah`}
                 role="Media & Communication Lead"
                 subRole="President, UIU App Forum"
                 imageSrc="/img/masud.jpg"
@@ -172,6 +185,7 @@ const Gratitude = () => {
             <div ref={(el) => (sections.current[12] = el)}>
               <TeamMember
                 name="S.M. NAYEMUR RAHMAN"
+                link={`https://www.facebook.com/TheAhaaGuy`}
                 role="Media & Communication Member"
                 subRole="Co-Head of PR, UIU App Forum"
                 imageSrc="/img/nayeem.jpg"
@@ -180,6 +194,7 @@ const Gratitude = () => {
             <div ref={(el) => (sections.current[13] = el)}>
               <TeamMember
                 name="Samia Sayeed"
+                link={`https://www.facebook.com/sayeedsamia00`}
                 role="Media & Communication Member"
                 subRole="Head of Media and Communication Department, UIU App Forum"
                 imageSrc="/img/samia.jpg"
@@ -189,6 +204,7 @@ const Gratitude = () => {
             <div ref={(el) => (sections.current[14] = el)}>
               <TeamMember
                 name="Shudipto Das"
+                link={`https://www.facebook.com/shudipto.das.98`}
                 role="Media & Communication Member"
                 subRole="General Secretary,UIU Computer Club"
                 imageSrc="/img/shudipto.jpg"
@@ -198,11 +214,12 @@ const Gratitude = () => {
           </div>
         </section>
         <section className="mb-16">
-          <h2 className="text-3xl font-bold text-center mb-8 text-orange-500">Outreach and Engagement Team</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <h2 className="font-zentry text-3xl font-bold text-center mb-8 text-orange-500">Outreach and Engagement Team</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <div ref={(el) => (sections.current[15] = el)}>
               <TeamMember
                 name="Samiul Haque Rudra"
+                link={`https://www.facebook.com/rudra.96824`}
                 role="Outreach and Engagement Lead"
                 subRole="President,UIU Computer Club"
                 imageSrc="/img/samiul.jpg"
@@ -211,6 +228,7 @@ const Gratitude = () => {
             <div ref={(el) => (sections.current[16] = el)}>
               <TeamMember
                 name="Akash Kumar Podder"
+                link={`https://www.facebook.com/akash.podder.9256`}
                 role="Outreach and Engagement Member"
                 subRole="Co-Head of PR, UIU App Forum"
                 imageSrc="/img/akash.jpg"
@@ -219,6 +237,7 @@ const Gratitude = () => {
             <div ref={(el) => (sections.current[17] = el)} >
               <TeamMember
                 name="Md. Rashique Hamjah Chowdhury "
+                link={`https://www.facebook.com/Meehal.BD`}
                 role="Outreach and Engagement Member"
                 subRole="Treasurer, UIU Robotics Club"
                 imageSrc="/img/rashique.jpg"
@@ -227,9 +246,20 @@ const Gratitude = () => {
             <div ref={(el) => (sections.current[18] = el)}>
               <TeamMember
                 name="Md.Naimur Rahman"
+                link={`https://www.facebook.com/mdnaimurrahman.navil?mibextid=ZbWKwL`}
                 role="Outreach and Engagement Member"
                 subRole="Head of R & D, Programing wing,UIU Computer Club"
                 imageSrc="/img/naimur3.jpg"
+              />
+            </div>
+
+            <div ref={(el) => (sections.current[19] = el)}>
+              <TeamMember
+                name="Faria Rakib Borsha"
+                link={`https://www.facebook.com/faria.rakib.borsha`}
+                role="Outreach and Engagement Member"
+                subRole="General Secretary, UIU Robotics Club"
+                imageSrc="/img/borsha.jpg"
               />
             </div>
 
--- src/components/TeamMember.jsx
+++ src/components/TeamMember.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const TeamMember = ({ name, role, subRole, imageSrc }) => {
+const TeamMember = ({ name, role, subRole, imageSrc, link }) => {
   return (
+    <Link to={link} >
     <div className="bg-gradient-to-br from-gray-900 to-black border border-orange-500/30 rounded-lg overflow-hidden transition-all duration-300 hover:border-orange-500 group">
       <div className="p-6">
         <div className="aspect-square mb-6 overflow-hidden rounded-xl bg-orange-500 relative">
@@ -11,13 +13,14 @@ const TeamMember = ({ name, role, subRole, imageSrc }) => {
             className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
           />
         </div>
-        <h3 className="text-xl font-bold text-orange-500">
+        <h3 className="font-zentry text-2xl font-bold text-orange-500">
           {name}
         </h3>
-        <p className="text-sm text-gray-300 mt-1">{role}</p>
-        {subRole && <p className="text-sm text-gray-400 mt-2">{subRole}</p>}
+        <p className="font-robert-regular text-sm text-gray-300 mt-1">{role}</p>
+        {subRole && <p className=" font-robert-regular text-sm text-gray-400 mt-2">{subRole}</p>}
       </div>
     </div>
+    </Link>
   );
 };
 
